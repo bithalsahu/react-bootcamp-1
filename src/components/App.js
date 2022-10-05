8756c6e9db58de61ab3fd7c638a1c5047913cc05
Bithal Sahu <bithalsahu484@gmail.com>
2022-10-06 02:33:00 +0530

    Replaced Project icon || Changed Branding || Added functional component header

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,29 @@
 import React, { Component } from 'react'
+import Header from './Bootstrap/Header';
 
+const InsideAppComponent = (props) => {
+  return (
+    <>
+    <p>Hello, this is an InsideAppComponent component, trying to use props</p>
+    <pre>{JSON.stringify(props, null, 2)}</pre>
+    </>
+  )
+} 
+
+// created using emmet command 'rcc'
 class App extends Component {
+  state = {
+    dark: false
+  }
   render() {
     return (
       <div className='App'>
-        <h1>Hello World!!!</h1>
-        <p>Test Render</p>
+        <Header dark={this.state.dark} >
+          Sample React Project!!!
+        </Header>
+        <p>Test text Render</p>
+        <InsideAppComponent attr1="Attribute1" attr2="Attribute2">
+        </InsideAppComponent>
       </div>
     )
   }
--- src/index.js
+++ src/index.js
@@ -4,11 +4,13 @@ import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+console.log('before render')
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+console.log('after render')
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
